4998507ba976b0164dffe403f3a0f6feac68912d
Matthew Esposito <matt.esposito@memfault.com>
2024-07-09 10:14:42 -0400

    fix(fuzz): fix clippy

--- fuzz/fuzz_targets/fuzz_target_1.rs
+++ fuzz/fuzz_targets/fuzz_target_1.rs
@@ -4,5 +4,5 @@ use libfuzzer_sys::fuzz_target;
 use zero_packet::packet::parser::PacketParser;
 
 fuzz_target!(|data: &[u8]| {
-    let parsed = PacketParser::parse(&data);
+    let _parsed = PacketParser::parse(&data);
 });
